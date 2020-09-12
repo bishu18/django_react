055199159511ecff1a97e098d71776ac22fb0f45
bishu18 <sourav.bishwajeet18@gmail.com>
2020-09-12 19:22:03 +0530

    Initial import

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+import Table from 'react-bootstrap/Table'
 
-function App() {
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/')
+    .then(res => {
+      console.log(res)
+      setItems(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+    // eslint-disable-next-line
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Corona Cases in different country</h1><br/>
+            <Table striped bordered hover>
+            <thead>
+              <tr>
+                  <th>#</th>
+                  <th>Country Name</th>
+                  <th>Country Code</th>
+                  <th>Total Cases</th>
+                  <th>Recovered Cases</th>
+                  <th>Death Cases</th>
+              </tr>
+              </thead>
+              <tbody>
+                {items.map(item => (
+                <tr key="item">
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.code}</td>
+                    <td>{item.total_cases}</td>
+                    <td>{item.recovered_cases}</td>
+                    <td>{item.death_cases}</td>
+                </tr>
+                ))}
+              </tbody>
+            </Table>
     </div>
   );
 }
-
-export default App;
